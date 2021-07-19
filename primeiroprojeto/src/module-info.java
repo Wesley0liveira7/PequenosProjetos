5146028cf5aa9c89cbb5c04c73bc21b4495bc1e8
Wesley0liveira7 <87623568+Wesley0liveira7@users.noreply.github.com>
2021-07-19 17:51:43 -0300

    Add files via upload

---
+++ primeiroprojeto/src/module-info.java
@@ -0,0 +1,15 @@
+module primeiroprojeto {
+	
+	package primeiroprojeto;
+
+	import java.util.Scanner;
+
+	public class mirosmar {
+
+		public static void main(String[] args) {
+			
+			Scanner teclado = new scanner(System.in);
+			
+			System.out.printlf("Digite um numero");
+			int i = teclado.nextInt();
+}
---
+++ primeiroprojeto/src/primeiroprojeto/mirosmar.java
@@ -0,0 +1,18 @@
+package primeiroprojeto;
+
+import java.util.Scanner;
+
+public class mirosmar {
+
+	public static void main(String[] args) {
+		
+		Scanner teclado = new scanner(System.in);
+		
+		System.out.printlf("Digite um numero");
+		int i = teclado.nextInt();
+		
+		//comando para ler um numero inteiro
+		
+	}
+
+}
